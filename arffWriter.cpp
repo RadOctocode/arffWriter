b13f872e24a58ee3f021047996dd8657fc9d65fb
RadOctocode <mtan2198@gmail.com>
2017-04-20 00:16:02 -0400

    very basically copies the file

--- arffWriter.cpp
+++ arffWriter.cpp
@@ -2,23 +2,46 @@
 
 
 
-	arffWriter::arffWriter(std::string my_file_name){
-		file_name = my_file_name;
+	arffWriter::arffWriter(std::string my_read_file, std::string my_write_file){
+      write_file.open(my_write_file);
+      read_file.open(my_read_file);
+
 	}//empty consrtcutor
 
 	void arffWriter::writeFile(){
-		std::cout<<file_name;
-		std::ifstream myfile (file_name);
-  		if (myfile.is_open()){
+  		if (read_file.is_open()){
   			std::string line;
-    		while ( getline (myfile,line) ){
-            	std::cout<<line<<'\n';
+    		while ( getline (read_file,line) ){
+            	write_file<<line<<'\n';
     		}
 
-    		myfile.close();
+    		read_file.close();
   		}
 
   		else{
     		std::cout << "Unable to open file";
   		} 
+
+  		write_file.close();
 	}//end of writeFile
+
+  /*int main ()
+{
+    ifstream infile("input.txt");
+    ofstream outfile("output.txt");
+    string content = "";
+    int i;
+
+    for(i=0 ; infile.eof()!=true ; i++) // get content of infile
+        content += infile.get();
+
+    i--;
+    content.erase(content.end()-1);     // erase last character
+
+    cout << i << " characters read...\n";
+    infile.close();
+
+    outfile << content;                 // output
+    outfile.close();
+    return 0;
+}*/
